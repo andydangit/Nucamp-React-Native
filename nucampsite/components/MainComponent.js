48f11699b63b821ae8485c5be3640c46f774f311
Andy Dang <andy.dang.it@gmail.com>
2020-02-04 10:38:49 -0600

    Components Part 2

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          campsites: CAMPSITES
+          campsites: CAMPSITES,
+          selectedCampsite: null
         };
     }
 
+    onCampsiteSelect(campsiteID) {
+        this.setState({selectedCampsite: campsiteID});
+    }
+
     render() {
-        return <Directory campsites={this.state.campsites} />;
+        return ( 
+        <View style={{flex: 1}}>
+            <Directory campsites={this.state.campsites} 
+                onPress={campsiteId => this.onCampsiteSelect(campsiteId)} />
+            <CampsiteInfo campsite={this.state.campsites.filter(
+                campsite => campsite.id === this.state.selectedCampsite)[0]} />
+        </View>
+        );
     }
 }
 
