50dc9e65c5517a26015799c0fd0138f237ec6891
Andy Dang <andy.dang.it@gmail.com>
2020-02-04 01:02:08 -0600

    Components Part 1

--- nucampsite/App.js
+++ nucampsite/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World! Andy </Text>
-    </View>
-  );
+    return (
+        <Main />
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0ff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
