cccd288d95d7a89a10393aacc9a61d4128678461
Andy Dang <andy.dang.it@gmail.com>
2020-02-03 23:23:24 -0600

    Starter App

--- nucampsite/App.js
+++ nucampsite/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World! Andy </Text>
     </View>
   );
 }
@@ -12,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0ff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
