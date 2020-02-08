03a20710b23418f6fe18fef57dca096a1cfbd60c
Andy Dang <andy.dang.it@gmail.com>
2020-02-08 11:57:39 -0600

    Assignment 1 Task 3

--- nucampsite/components/AboutComponent.js
+++ nucampsite/components/AboutComponent.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements'
+import { View, Text, ScrollView, FlatList} from 'react-native';
+import { Card, ListItem } from 'react-native-elements'
+import { PARTNERS } from '../shared/partners.js';
+
+
+function Mission({}) {
+
+        return (
+            <Card title="Our Mission" >
+                <Text>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                </Text>
+            </Card>
+        );
+    };
+
 
 class About extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            partners: PARTNERS,
+        }
+    }
 
     static navigationOptions  = {
         title: "About Us"
@@ -11,13 +31,32 @@ class About extends Component {
 
 
     render () {
+
+        const renderPartners = ({ item }) => {
+            return (
+                <ListItem 
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+                />
+            );
+        };
+
+
         return (
             <ScrollView>
-
-
+                <Mission />
+                <Card title="Community Partners">
+                    <FlatList
+                        data={this.state.partners}
+                        renderItem={renderPartners}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </Card> 
             </ScrollView>
         );
     }
 }
+
 
 export default About;
