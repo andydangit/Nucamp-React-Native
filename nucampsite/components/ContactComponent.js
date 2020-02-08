f7612c344b9e3d3dae5eada3a3c36cba3b7424ff
Andy Dang <andy.dang.it@gmail.com>
2020-02-08 10:30:42 -0600

    Assignment 1 task 1

---
+++ nucampsite/components/ContactComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class Contact extends Component {
+
+    static navigationOptions  = {
+        title: "Contact Us"
+    }
+
+    render () {
+        return (
+            <ScrollView>
+
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
-        CampsiteInfo: { screen: CampsiteInfo }
+        CampsiteInfo: { screen: CampsiteInfo },
+        About: { screen: About},
+        Contact: { screen: Contact }
     },
     {
         initialRouteName: 'Directory',
@@ -41,15 +45,56 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const CopntactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: CopntactNavigator }
+
     },
     {
         drawerBackgroundColor: '#CEC8FF'
     }
 );
+
+
 
 class Main extends Component {
     render() {
