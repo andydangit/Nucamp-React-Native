8890c174c8b5757e2a2235d38d89e649cad6bd7b
Andy Dang <andy.dang.it@gmail.com>
2020-02-14 20:42:41 -0600

    Activity Indicator

--- nucampsite/components/AboutComponent.js
+++ nucampsite/components/AboutComponent.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView, FlatList} from 'react-native';
 import { Card, ListItem } from 'react-native-elements'
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 
 const mapStateToProps = state => {
@@ -32,17 +33,39 @@ class About extends Component {
     }
 
 
-    render () {
-
-        const renderPartners = ({ item }) => {
+    render() {
+        const renderPartner = ({item}) => {
             return (
-                <ListItem 
+                <ListItem
                     title={item.name}
                     subtitle={item.description}
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
 
 
         return (
