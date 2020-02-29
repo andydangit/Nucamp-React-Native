b732c79f3098f3e1a747d93cf24f88361d61c375
Andy Dang <andy.dang.it@gmail.com>
2020-02-29 12:17:17 -0600

    Task 3 Fin

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -338,18 +338,23 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios') ?
-                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
         });
     }
+
+//Task 3
+    async showNetInfo() {
+        const connectionInfo = await NetInfo.fetch();
+            (Platform.OS === 'ios') ?
+                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+        console.log(connectionInfo);
+    }
+//End
 
     componentWillUnmount() {
         this.unsubscribeNetInfo();
